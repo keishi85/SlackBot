e71f03a724e0a7ce97385a4a014459f666d80dfe
Keishi Kaneko <iotegn85@gmail.com>
2024-11-05 15:18:53 +0900

    Update groups.js

--- groups.js
+++ groups.js
@@ -1,5 +1,5 @@
-const groupA = ['王慧涛', 'Yoshito MIKADO', '服部瑞生', 'ディエゴ', '田中一輝', '金子慧士', '安藤颯', '村山挙太', '古俣朝陽'];
-const groupB = ['LIZhe', '森下裕咲子', '鈴木萌仁伽', '宮沢知穂', '大瀧陽', '根本大雅', '林知歩', '白井正真'];
+const groupA = [];
+const groupB = [];
 //'Linman Shen'
 
 exports.getGroup = (name) => {
